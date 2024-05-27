22cc7b8edc0339b0c652a29aab24e8771d37bbfb
KhanyiiK <2433380@students.wits.ac.za>
2024-05-27 17:20:17 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Product from "./pages/Product";
+import Home from "./pages/Home";
+import ProductList from "./pages/ProductList";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Cart from "./pages/Cart";
+import BlogPage from "./pages/BlogPage";
+import ImageSlider from "./pages/ImageSlider";
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/product" element={<Product />} />
+        <Route path="/productlist" element={<ProductList />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/blog" element={<BlogPage />} />
+        <Route path="/imageslider" element={<ImageSlider />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
